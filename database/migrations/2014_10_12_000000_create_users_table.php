190e273a604818b934ce173d7bc83b7e8d310bc9
r1d3rzz <myintthwaykhine127@gmail.com>
2023-12-28 11:12:02 +0630

    setup auth feature with laravel:breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('department_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
+            $table->string('address')->nullable();
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->string('avatar')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('date_of_join')->nullable();
+            $table->string('nrc_number')->unique()->nullable();
+            $table->boolean('is_present')->default(true);
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
